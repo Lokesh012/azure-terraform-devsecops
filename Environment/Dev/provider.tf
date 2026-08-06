9c132ae539a80c40ef1acc568dcdec67862cb889
Lokesh Srivastav <lokeshsrivastav93@gmail.com>
2026-08-06 17:35:39 +0530

    Installing and Validating Terraform

--- Environment/Dev/provider.tf
+++ Environment/Dev/provider.tf
@@ -8,11 +8,11 @@ terraform {
   }
 
   backend "azurerm" {
-  resource_group_name  = "backend-rg1"
-  storage_account_name = "cvbackendst79059"
-  container_name       = "backendcont03"
-  key                  = "dev.terraform.tfstate"
-}
+    resource_group_name  = "backend-rg1"
+    storage_account_name = "cvbackendst79059"
+    container_name       = "backendcont03"
+    key                  = "dev.terraform.tfstate"
+  }
 
 }
 
